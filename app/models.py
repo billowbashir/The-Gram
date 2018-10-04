c47fab77c39ad5a47c6684aa1c9b8db3948e35db
billowbashir <billowbashir@gmail.com>
2018-10-04 14:56:06 +0300

    create models for images and comments

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    comment=models.CharField(max_length=60)
+
+
+class Image(models.Model):
+    image=models.ImageField(upload_to='photos/')
+    caption=models.CharField(max_length=60)
+    likes=models.IntegerField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    comments=models.ForeignKey('Comment')
+
+    class Meta:
+        ordering=['post_date']
