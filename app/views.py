1aa87cdefd91ab20d601b006b904d81ec4420a6c
billowbashir <billowbashir@gmail.com>
2018-10-04 19:10:13 +0300

    display images

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Image
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request,'index.html')
+    images=Image.objects.all()
+    return render(request,'index.html',{'images':images,})
